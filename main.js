6d80c1d6b48b9fed86fbe111d6189f257fe21ac0
Jacob <jakeboone@aol.com>
2014-07-08 20:12:08 -0600

    trying to add rating stars

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ $(document).on('ready', function() {
 	$(document).on('click', '.sub-button', function(){
 		var quoteText = $('#inputfield').val();
 		var	authorText = $('.author').val();
-		var quoteBox = $('<div class="quotebody"><p class="a-quote">Quote: ' + quoteText + '</p><p class="an-author">Character: '+ authorText +'</p><button class="deletequote">Delete</button><div class="rating"></div></div>');
+		var quoteBox = $('<div class="quotebody"><p class="a-quote">Quote: ' + quoteText + '</p><p class="an-author">Character: '+ authorText +'</p><button class="deletequote">Delete</button><fieldset class="rating"><legend>Please rate:</legend><input type="radio" id="star5" name="rating" value="5" /><label for="star5" title="Rocks!">5 stars</label><input type="radio" id="star4" name="rating" value="4" /><label for="star4" title="Pretty good">4 stars</label><input type="radio" id="star3" name="rating" value="3" /><label for="star3" title="Meh">3 stars</label><input type="radio" id="star2" name="rating" value="2" /><label for="star2" title="Kinda bad">2 stars</label><input type="radio" id="star1" name="rating" value="1" /><label for="star1" title="Sucks big time">1 star</label></fieldset></div>');
 		// input a hover function then display the delete button
 
 
@@ -19,9 +19,7 @@ $(document).on('ready', function() {
 				// background: #00FF00,
 				// // fontSize: 100,
 				// }) 
-			$(quoteBox).css({
-				backgroundColor: white,
-			})
+		
 
 
 
@@ -35,6 +33,8 @@ $(document).on('ready', function() {
 
 
 // for the Stars make a ul li list see http://rating-widget.com/get/rating/blogger/
+
+
 
 
 
