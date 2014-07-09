59f6d8eab0c39363b88ad7e5a6c85aa44109e357
Jacob <jakeboone@aol.com>
2014-07-09 17:08:40 -0600

    pitiful updates

---
+++ data.js
@@ -0,0 +1,44 @@
+// Define variable to store data - template courtesy of JMCARLSON on git hub
+var quoteData = [];
+
+quoteData.push({
+	text: 'Sucking at something is the first step to being sorta good at something',
+	author: 'Jake the Dog',
+	views: 0,
+	rating: 0
+	});
+quoteData.push({
+	text: 'When bad things happen, I know you want to believe they are a joke, but sometimes life is scary and dark. That is why we must find the light',
+	author: 'BMO',
+	views: 0,
+	rating: 0
+	});
+quoteData.push({
+	text: 'I know who I am and I’ll know what I want if and when it comes along',
+	author: 'Fionna the Human',
+	views: 0,
+	rating: 0
+	});
+quoteData.push({
+	text: 'People make mistakes. It’s a part of growing up, and you never really stop growing',
+	author: 'Duke of Nuts',
+	views: 0,
+	rating: 0
+	});
+quoteData.push({
+	text: 'Sometimes you want someone and you want to kiss and be with them, but you can’t because responsibility demands sacrifice.',
+	author: 'Princess Bubblegum',
+	views: 0,
+	rating: 0
+	});
+quoteData.push({
+	text: 'Even if we like each other, we’re still going to hurt each other',
+	author: 'Fire Princess',
+	views: 0,
+	rating: 0
+	});
+
+// create object - uniqueid - put on master array
+// when delted, put on delted array
+// undo pull last item from deleted array
+// 
--- main.js
+++ main.js
@@ -1,7 +1,44 @@
+// var renderData = function(layoutStyle) {
+// 	// console.log("Rendering the data with layout style: " + layoutStyle);
+// 	$('.data').empty();
+// 	console.log(quoteData);
+// 	for (var i = 0; i <= quoteData.length - 1; i++) {
+
+// 		var newDataItem = $('<div class="quote-data">');
+// 		newDataItem.append(
+// 			'<div>' + quoteData[i].text + '</div>'
+// 		);
+// 		newDataItem.append(
+// 			'<div>-' + quoteData[i].author + '</div>'
+// 		);
+// 		newDataItem.append(
+// 			'<div>Rating: ' + quoteData[i].rating + '</div>'
+// 		);
+// 		newDataItem.append(
+// 			'<img id="button-remove" src="http://placehold.it/20&text=Remove">'
+// 		);
+// 		// Create new DOM element
+// 		newDataItem.appendTo('.data');
+
+// 	};
+// }
+
+
+
+
+
+
+
+
 
 // when var's are outside of function, only 1 quote is added. WHY?? when inside it is unlimited.
 var undoButton = $('<button class="undoDelete" ><img src="http://a.deviantart.net/avatars/s/i/sillyicekingplz.gif?1"></button>')
+var	randomButton = $('<button class="randomQuote"><img src="http://fc02.deviantart.net/fs70/f/2011/220/a/1/poo_brain_horse_icon_by_supajackle-d45x9b2.gif"></button>')
+var arrQuotes = []
 
+
+
+$('.inputbox').append(randomButton)
 // var undid = $('')
 // var deletedQuote = $(undid)
 
@@ -17,6 +54,7 @@ var undoButton = $('<button class="undoDelete" ><img src="http://a.deviantart.ne
 		var theAuthor = $('<p class="an-author">Character: '+ authorText +'</p>')
 		var deleteButton = $('<button class="deletequote">Delete</button>')
 		var ratingBar = $('<div class="rating"><legend>Mathmatical-ness!!!:</legend><input type="radio" id="star5" name="rating" value="5" /><label for="star5" title="Rocks!">5 stars</label><input type="radio" id="star4" name="rating" value="4" /><label for="star4" title="Pretty good">4 stars</label><input type="radio" id="star3" name="rating" value="3" /><label for="star3" title="Meh">3 stars</label><input type="radio" id="star2" name="rating" value="2" /><label for="star2" title="Kinda bad">2 stars</label><input type="radio" id="star1" name="rating" value="1" /><label for="star1" title="Sucks big time">1 star</label></div>')
+
 		
 		
 
@@ -27,8 +65,11 @@ var undoButton = $('<button class="undoDelete" ><img src="http://a.deviantart.ne
 		$(quoteBox).append(textBox, itemBox)
 		$(textBox).append(theQuote, theAuthor)
 		$(itemBox).append(ratingBar, deleteButton)
+
+
 		
 		$('.inputbox').after(quoteBox)
+		$(arrQuotes).push(quoteBox)
 			console.log(quoteText,"-", authorText)
 		
 
@@ -46,11 +87,10 @@ var undoButton = $('<button class="undoDelete" ><img src="http://a.deviantart.ne
 			$('.inputbox').append(undoButton)
 	   
 
-			// quote reappears when undo button is clicked
+// Quote reappears when undo button is clicked
 			$(document).on('click', '.undoDelete', function(){
 				// this actually reshows ALL deleted items, not just the last one
-				storeQuote.show().last()
-				
+				storeQuote.show()
 				$('.inputbox').after(storeQuote)
 
 				console.log('undo')
@@ -60,6 +100,14 @@ var undoButton = $('<button class="undoDelete" ><img src="http://a.deviantart.ne
 
 
 
+// random qoute button
+			$(document).on('click', '.randomQuote', function(){
+				
+				alert(arrQuotes[2])
+				
+
+				console.log('undo')
+			});
 // for the Stars make a ul li list see http://rating-widget.com/get/rating/blogger/
 
 
